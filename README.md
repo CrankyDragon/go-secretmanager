17c5d1546eea9df5e4cab9621d9b7c4a1438a0f6
Shaun Harding <shaun@tvtime.com>
2019-03-20 16:14:33 -0700

    stripping out uneeded logic

--- README.md
+++ README.md
@@ -5,11 +5,6 @@ Simple command line for fetching secrets from AWS Secrets Manager
 #### Command line Arguments
 Currently supported
 ```
-  -c string
-        Full path to credentials file (default "~\.aws\credentials")
-  -k    Skip profile check and just use default for use when no cred file and default will work
-  -p string
-        Profile to use (default "default")
   -s string
         Secret To Fetch (default "secret")
   -r string
--- main.go
+++ main.go
@@ -5,10 +5,7 @@ import (
 	"flag"
 	"fmt"
 	"log"
-	"os/user"
-	"path/filepath"
 
-	"github.com/alyu/configparser"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/aws/session"
@@ -16,74 +13,20 @@ import (
 )
 
 func main() {
-	sourceProfile := flag.String("p", "default", "Profile to use")
 	secret := flag.String("s", "secret", "Secret To Fetch")
-	version := flag.String("v", "version", "Version of secret To Fetch")
-	skipProfile := flag.Bool("k", false, "Skip profile check and just use default for use when no cred file and default will work")
-	credFile := flag.String("c", filepath.Join(getCredentialPath(), ".aws", "credentials"), "Full path to credentials file")
 	region := flag.String("r", "us-east-1", "AWS Region")
+	version := flag.String("v", "version", "Version of secret To Fetch")
 	flag.Parse()
 	if *secret == "secret" {
 		fmt.Println("You must specify a secret name to fetch")
 		return
 	}
 
-	var sess *session.Session
-	if *skipProfile {
-		//Use Default Credentials
-		sess = session.Must(session.NewSession())
-	} else {
-		//Get Specified Credentials
-		exists, err := checkProfileExists(credFile, sourceProfile)
-		if err != nil || !exists {
-			fmt.Println(err.Error())
-			return
-		}
-		sess = CreateSession(sourceProfile, region)
-	}
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		Config: aws.Config{Region: region},
+	}))
 
 	getSecret(sess, secret, version)
-}
-
-// CreateSession Creates AWS Session with specified profile
-func CreateSession(profileName, region *string) *session.Session {
-	profileNameValue := *profileName
-	sess := session.Must(session.NewSessionWithOptions(session.Options{
-		Profile: profileNameValue,
-		Config:  aws.Config{Region: region},
-	}))
-	return sess
-}
-
-// getCredentialPath returns the users home directory path as a string
-func getCredentialPath() string {
-	usr, err := user.Current()
-	if err != nil {
-		log.Fatal(err)
-	}
-	return usr.HomeDir
-}
-
-// checkProfileExists takes path to the credentials file and profile name to search for
-// Returns bool and any errors
-func checkProfileExists(credFile *string, profileName *string) (bool, error) {
-	config, err := configparser.Read(*credFile)
-	if err != nil {
-		fmt.Println("Could not find credentials file")
-		fmt.Println(err.Error())
-		return false, err
-	}
-	section, err := config.Section(*profileName)
-	if err != nil {
-		fmt.Println("Could not find profile in credentials file")
-		return false, nil
-	}
-	if !section.Exists("aws_access_key_id") {
-		fmt.Println("Could not find access key in profile")
-		return false, nil
-	}
-
-	return true, nil
 }
 
 func getSecret(sess *session.Session, secretName *string, secretVersion *string) {
